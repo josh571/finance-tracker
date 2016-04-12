f5ed2915cad61398aca60f60b97efad58aadeae9
Josh N <josh571@users.noreply.github.com>
2016-04-11 22:40:41 -0400

    bug fix

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,8 +8,8 @@ class Stock < ActiveRecord::Base
 		looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
 		return nil unless looked_up_stock.name
 
-		new_stock = new(ticker: looked_up_stock, name: looked_up_stock.name)
-		new_stock.last_price = new_stock.last_price
+		new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
+		new_stock.last_price = new_stock.price
 		new_stock
 	end
 
